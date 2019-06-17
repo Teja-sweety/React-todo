79937e0e32eacd826c12d3536a5b6306de7f040b
vinay0458 <vinay0458@gmail.com>
2019-06-16 23:33:56 -0700

    new plug in added

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Button} from 'primereact/button';
+import {InputText} from 'primereact/inputtext';
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <span className="p-float-label">
+        <InputText id="in" value="123" />
+        <label htmlFor="in">Username</label>
+    </span>
+      <Button label="Save" />
     </div>
   );
 }
